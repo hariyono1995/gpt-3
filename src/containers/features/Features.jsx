2554e3f6984dc939daef35ee86f7ccfdf259fbfe
hariyono1995 <onnelikrio@gmail.com>
2022-06-19 11:46:32 +0700

    ...

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+
+import Feature from "components/feature/Feature";
 import "./features.css";
 
 const featuresData = [
@@ -21,6 +23,25 @@ const featuresData = [
 ];
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">
+          The Future is Now and You Just Need to Realize It. Step into Future
+          Today. & Make it Happen.
+        </h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="gpt3__features-container">
+        {featuresData.map((item, index) => (
+          <Feature
+            title={item.title}
+            text={item.text}
+            key={item.title + index}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Features;
